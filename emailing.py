60a65bd16cb2b71a323e8c1fc8e8e9f66f328311
Swapnil <swapnilacharjee1724@gmail.com>
2026-02-06 12:23:08 +0530

    Choose best image, clean images folder, implement send email functionality with google app password, improve security of code by using environment variables.

--- emailing.py
+++ emailing.py
@@ -1,2 +1,30 @@
-def send_email():
-    print("email sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+import os
+
+PASSWORD = os.getenv("GOOGLE_WEBCAM_TOKEN")
+SENDER = "pplctnproject@gmail.com"
+RECEIVER = "pplctnproject@gmail.com"
+
+if PASSWORD is None:
+    raise RuntimeError("Environment variable GOOGLE_WEBCAM_TOKEN is missing. Please set the environment variable")
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New object has been detected"
+    email_message.set_content("The webcam just detected a new object")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/10.png")
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import cv2
 import time
+import glob
+import os
 from emailing import send_email
+
+def clean_imagefolder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -8,6 +15,7 @@ time.sleep(1)
 first_frame = None
 
 status_list = []
+count = 1
 while True:
     status = 0
     check, frame = video.read()
@@ -32,6 +40,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
@@ -39,7 +52,8 @@ while True:
     print(status_list)
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_imagefolder()
 
     cv2.imshow("Video", frame)
 
